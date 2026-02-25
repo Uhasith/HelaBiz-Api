a55a954ab57fc11157a63932a0cf994527f6b867
Hasith Udayanga <hasithudayanga@192.168.1.8>
2026-02-25 12:19:45 +0530

    invoice fix

--- app/Http/Controllers/Api/OrderInvoiceController.php
+++ app/Http/Controllers/Api/OrderInvoiceController.php
@@ -271,17 +271,13 @@ class OrderInvoiceController extends Controller
             ->currencySymbol($this->getCurrencySymbol($tenant->currency ?? 'USD'))
             ->currencyFormat('{SYMBOL} {VALUE}')
             ->addItems($items)
-            ->taxableAmount($order->subtotal)
             ->setCustomData(['order_number' => $order->order_number])
             ->filename("invoice_{$invoiceNumber}");
 
-        // Add discount if present
+        // Add discount if present - package will auto-calculate final total
         if ($order->discount > 0) {
             $invoice->totalDiscount($order->discount);
         }
-
-        // Set final total amount (after discount)
-        $invoice->totalAmount($order->total);
 
         // Add tax if present
         // if ($order->tax > 0) {
